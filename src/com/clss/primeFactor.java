a37c1c5f137f3d2ccbea76564390915c1c5a706e
luisOlvera <luis.olvera@tralix.com>
2015-02-11 18:30:56 -0600

    kata PrimeFactor con Tpp

---
+++ src/com/clss/primeFactor.java
@@ -0,0 +1,17 @@
+package com.clss;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by luisolvera on 11/02/15.
+ */
+public class primeFactor {
+     List<Integer> multiplicadores = new ArrayList<Integer>();
+
+    private void generate(int numero){
+
+
+    }
+
+}
---
+++ src/com/test/primeFactorTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class primeFactorTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+    @Test
+    public  void nothing ()throws Exception {
+
+
+    }
+}
